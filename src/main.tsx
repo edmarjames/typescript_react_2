0180b80d8bb107f5dd87aba78240370eaec0e9e9
edmar.james <edmarjamesbautista@gmail.com>
2025-01-16 16:12:22 +0800

    Install redux toolkit, react-redux and setup store

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@
 import { StrictMode }                  from 'react';
 import { createRoot }                  from 'react-dom/client';
 
+// external imports
+import { Provider }                    from 'react-redux';
+
 // internal imports
 import App                             from './App.tsx';
+import { store }                       from './store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </Provider>
 )
