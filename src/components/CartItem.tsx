b96d804b09ac4d2315e32b676d935cea146f920f
edmar.james <edmarjamesbautista@gmail.com>
2025-01-20 18:23:20 +0800

    Create removeItem action on cartSlice and dispatch it using useDispatch

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { type CartItems as CartItemProps } from "../utils/types";
+import { removeItem } from '../features/cart/cartSlice';
+
+import { useDispatch } from 'react-redux';
 
 
 export default function CartItem({ id, img, title, price, amount}: CartItemProps) {
+
+  const dispatch = useDispatch();
 
   return (
     <article className='cart-item'>
@@ -10,7 +15,7 @@ export default function CartItem({ id, img, title, price, amount}: CartItemProps
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className='remove-btn'>remove</button>
+        <button className='remove-btn' onClick={() => dispatch(removeItem(id))}>remove</button>
       </div>
       <div>
         <button className='amount-btn'>Up</button>
